1871b4e54a4d365a53e0c925678a8833e69c0e74
allen <1043787207@qq.com>
2018-07-05 21:07:08 +0800

    跨域信息设置

--- MxShop/settings.py
+++ MxShop/settings.py
@@ -51,9 +51,12 @@ INSTALLED_APPS = [
 	'xadmin',
 	'rest_framework',
 	'django_filters',
+	'corsheaders',
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
+	'django.middleware.common.CommonMiddleware',
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
@@ -62,6 +65,8 @@ MIDDLEWARE = [
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'MxShop.urls'
 
